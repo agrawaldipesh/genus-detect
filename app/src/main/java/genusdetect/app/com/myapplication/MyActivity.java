8fbff70e7d358e4985c779b9760e0e74e2a59d79
iuriep <iurie.potorac@gmail.com>
2014-11-06 23:27:26 +0100

    test first comit

---
+++ app/src/main/java/genusdetect/app/com/myapplication/MyActivity.java
@@ -0,0 +1,57 @@
+package genusdetect.app.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Integer total = 5+9;
+
+        Util util = new Util();
+
+        util.suma(5,61);
+
+        super.onCreate(savedInstanceState);
+
+        super.setContentView(R.layout.activity_my);
+        Integer total1 = suma(5, 9);
+
+    }
+
+    public Integer suma(Integer a, Integer b) {
+
+        return a+b;
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+       // fa altfel
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/genusdetect/app/com/myapplication/Util.java
@@ -0,0 +1,12 @@
+package genusdetect.app.com.myapplication;
+
+/**
+ * Created by Dedea on 6/11/2014.
+ */
+public class Util {
+
+    public Integer suma(Integer a, Integer b) {
+
+        return a+b;
+    }
+}
